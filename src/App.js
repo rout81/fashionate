fa639f3b039598dd98d27309a7c3b5f00508882a
satanrout <satyaranjanrout81@gmail.com>
2020-07-11 16:31:52 +0530

    backing up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
+import { Route } from "react-router-dom";
+
+const Hatspage = () => (
+  <div>
+    <h1>hats lelo</h1>
+  </div>
+);
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Homepage />
+        <Route exact path={"/"} component={Homepage} />
+        <Route exact path={"/hats"} component={Hatspage} />
       </div>
     );
   }
