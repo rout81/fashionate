bc8bb3be468a97f61ff7cd5292bd33497ccb20b3
satanrout <satyaranjanrout81@gmail.com>
2020-07-23 11:36:14 +0530

    taki

--- src/App.js
+++ src/App.js
@@ -3,18 +3,29 @@ import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
 import { Route } from "react-router-dom";
 
-const Hatspage = () => (
-  <div>
-    <h1>hats lelo</h1>
-  </div>
-);
+const HomePage = () => {
+  return (
+    <div>
+      <h1>homepage</h1>
+    </div>
+  );
+};
+
+const TopicsPage = () => {
+  return (
+    <div>
+      <h1>Topics Page</h1>
+    </div>
+  );
+};
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Route exact path={"/"} component={Homepage} />
-        <Route exact path={"/hats"} component={Hatspage} />
+        {/* <Route exact path={"/"} component={Homepage} /> */}
+        <Route exact path={"/homepage"} component={HomePage} />
+        <Route exact path={"/topics"} component={TopicsPage} />
       </div>
     );
   }
